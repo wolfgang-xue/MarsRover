2466b5f857332a009259e1120000354fa46f3a01
wolfgang-xue <wolfgangxue@gmail.com>
2019-05-09 22:36:33 +1000

    Added model and service

---
+++ MarsRover/Services/IRoverService.cs
@@ -0,0 +1,13 @@
+﻿using MarsRover.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRover.Services
+{
+    interface IRoverService
+    {
+        RoverPosition Navigate(RoverPosition start, string command);
+    }
+}
---
+++ MarsRover/Services/RoverService.cs
@@ -0,0 +1,22 @@
+﻿using MarsRover.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRover.Services
+{
+    public class RoverService : IRoverService
+    {
+
+        public RoverService()
+        {
+
+        }
+
+        public RoverPosition Navigate(RoverPosition start, string command)
+        {
+            return new RoverPosition();
+        }
+    }
+}
